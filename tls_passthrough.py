268d272b97496ec0f29c2d6becb390c77d3ce490
wuqiong <wuqiong@sina.com>
2021-04-25 03:12:28 +0800

    完善脚本

--- tls_passthrough.py
+++ tls_passthrough.py
@@ -131,10 +131,10 @@ def next_layer(next_layer):
     This hook does the actual magic - if the next layer is planned to be a TLS layer,
     we check if we want to enter pass-through mode instead.
     """
-    if isinstance(next_layer, TlsLayer) and next_layer._client_tls:
+    if isinstance(next_layer, TlsLayer) and next_layer._client_tls and 'soulapp.cn' in repr(next_layer.server_conn.address) :
         server_address = next_layer.server_conn.address
-
-        if tls_strategy.should_intercept(server_address) and 'soulapp.cn' in repr(next_layer.server_conn.address):
+        
+        if tls_strategy.should_intercept(server_address) :
             # We try to intercept.
             # Monkey-Patch the layer to get feedback from the TLSLayer if interception worked.
             next_layer.__class__ = TlsFeedback
