748848c313e3c0097096d63dfd8190078a40943f
wuqiong <wuqiong@sina.com>
2021-04-25 22:37:01 +0800

    配置查询ip网页

--- httpproxy/soul/login/login_class.py
+++ httpproxy/soul/login/login_class.py
@@ -15,11 +15,12 @@ class Counter:
         self.get_smDeviceId_ios = "http://fp-it.fengkongcloud.com/v3/profile/ios"
         self.get_smDeviceId_conf = 'http://fp-it.fengkongcloud.com/v3/cloudconf'
         self.get_smDeviceId_android = 'http://fp-it.fengkongcloud.com/v3/profile/android'
-        self.ip = 'ip138.com'
+        self.ip = 'www.ip111.cn'
     
     def request(self, flow: mitmproxy.http.HTTPFlow):
         if flow.request.pretty_url == self.get_smDeviceId_ios or flow.request.pretty_url == self.get_smDeviceId_android \
-                or flow.request.pretty_url == self.get_smDeviceId_conf or "soul" in flow.request.host:
+                or flow.request.pretty_url == self.get_smDeviceId_conf or "soul" in flow.request.host \
+                or self.ip in flow.request.pretty_url:
             pass
         else:
             flow.response = http.HTTPResponse.make(404)
