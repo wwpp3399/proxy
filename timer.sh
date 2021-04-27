e326c78b1857913b44b58dfdd2d0796fb6e18101
wuqiong <wuqiong@sina.com>
2021-04-27 18:44:07 +0800

    更新脚本

--- timer.sh
+++ timer.sh
@@ -5,7 +5,7 @@ killall mitmdump
 killall mitmproxy
 killall mitmweb
 cd ~/HttpProxy
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 -q &
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 &
 sleep 10
 done
 
