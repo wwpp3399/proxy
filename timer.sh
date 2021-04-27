58f28eae28225a8597821ed326c0e019ef5cd401
wuqiong <wuqiong@sina.com>
2021-04-27 18:39:09 +0800

    修改时间间隔

--- timer.sh
+++ timer.sh
@@ -6,6 +6,6 @@ killall mitmproxy
 killall mitmweb
 cd ~/HttpProxy
 mitmproxy -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080
-sleep 600
+sleep 10
 done
 
