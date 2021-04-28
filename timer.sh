706a4f709bf2e8ddbbc90a738c755f9b5aabdbe1
wuqiong <wuqiong@sina.com>
2021-04-28 20:42:08 +0800

    多进程

--- timer.sh
+++ timer.sh
@@ -5,7 +5,10 @@ killall mitmdump
 killall mitmproxy
 killall mitmweb
 cd ~/HttpProxy
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 &
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 & \
 sleep 600
 done
 
