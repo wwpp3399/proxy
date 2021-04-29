44d2593ef6c6a38ecb36266d5c3dc2104646c156
wuqiong <wuqiong@sina.com>
2021-04-29 21:36:33 +0800

    自动ip

--- timer.sh
+++ timer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-proxy=$1
+proxy=echo curl http://47.116.12.2/getip?num=1&type=1&pro=&city=0&yys=0&port=11&pack=113186&ts=0&ys=0&cs=0&lb=4&sb=0&pb=4&mr=1&regions=&username=chukou01&spec=1
 while true
 do
 killall mitmdump
 killall mitmproxy
 killall mitmweb
 cd ~/HttpProxy
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 --mode upstream:http://$proxy/ & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 --mode upstream:http://$proxy/ & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 --mode upstream:http://$proxy/ & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 --mode upstream:http://$proxy/ & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 --mode upstream:https://$proxy/ & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 --mode upstream:https://$proxy/ & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 --mode upstream:https://$proxy/ & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 --mode upstream:https://$proxy/ & \
 sleep 600
 done
 
