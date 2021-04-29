c04515231b299ed269f8439c7b30c4cf8034b314
wuqiong <wuqiong@sina.com>
2021-04-29 20:53:13 +0800

    新增代理支持

--- timer.sh
+++ timer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+proxy=$1
 while true
 do
 killall mitmdump
 killall mitmproxy
 killall mitmweb
 cd ~/HttpProxy
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 upstream:https://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 upstream:https://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 upstream:https://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 upstream:https://$proxy & \
 sleep 600
 done
 
