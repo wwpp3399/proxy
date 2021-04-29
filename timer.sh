62964f55ce0da31f1bf7e41dee7123d82102e939
wuqiong <wuqiong@sina.com>
2021-04-29 21:08:12 +0800

    修正脚本

--- timer.sh
+++ timer.sh
@@ -6,10 +6,10 @@ killall mitmdump
 killall mitmproxy
 killall mitmweb
 cd ~/HttpProxy
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 --mode upstream:https://$proxy & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 --mode upstream:https://$proxy & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 --mode upstream:https://$proxy & \
-mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 --mode upstream:https://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5080 --mode upstream:http://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 --mode upstream:http://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 --mode upstream:http://$proxy & \
+mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 --mode upstream:http://$proxy & \
 sleep 600
 done
 
