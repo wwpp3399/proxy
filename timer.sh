4339cfa59031ba8a0d8d4efa5e9ad81eb46fe07d
wuqiong <wuqiong@sina.com>
2021-05-01 01:49:52 +0800

    修改timer脚本

--- timer.sh
+++ timer.sh
@@ -4,8 +4,7 @@ echo "参数是${type}"
 if [[ $type == "" ]]; then
 	echo "执行远程代理"
 	proxy=$(curl http://47.116.12.2/getip?num=1&type=1&pro=&city=0&yys=0&port=11&time=2&ts=0&ys=0&cs=0&lb=4&sb=0&pb=4&mr=1&regions=&username=chukou01&spec=1)
-	while true
-	do
+	
 	killall mitmdump
 	killall mitmproxy
 	killall mitmweb
@@ -14,13 +13,11 @@ if [[ $type == "" ]]; then
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 --mode upstream:https://$proxy/ & \
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 --mode upstream:https://$proxy/ & \
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 --mode upstream:https://$proxy/ & \
-	sleep 300
-	done
+	
 elif [[ $type == 'v' ]]; then
 	#statements
 	echo "执行无代理模式"
-	while true
-	do
+	
 	killall mitmdump
 	killall mitmproxy
 	killall mitmweb
@@ -29,12 +26,10 @@ elif [[ $type == 'v' ]]; then
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090  & \
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010  & \
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020  & \
-	sleep 300
-	done
+	
 else
 	echo "执行指定代理模式"
-	while true
-	do
+	
 	killall mitmdump
 	killall mitmproxy
 	killall mitmweb
@@ -43,7 +38,6 @@ else
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5090 --mode upstream:https://$1/ & \
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5010 --mode upstream:https://$1/ & \
 	mitmdump -s main.py -s tls_passthrough.py --set client_certs=cer/soul.pem --set block_global=false -p5020 --mode upstream:https://$1/ & \
-	sleep 300
-	done
+	
 fi
 	#statements
